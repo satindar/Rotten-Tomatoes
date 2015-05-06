9c2100937f9b2d0b582f730c82acc8fb740e72c6
satindar <satindar@gmail.com>
2015-05-06 13:53:00 -0700

    Initial commit

---
+++ Rotten Tomatoes/MoviesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MoviesViewController.swift
+//  Rotten Tomatoes
+//
+//  Created by Satindar Dhillon on 5/6/15.
+//  Copyright (c) 2015 Jaya. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=us")!
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+            
+            if let json = json {
+                self.movies = json["movies"] as? [NSDictionary]
+            }
+            self.tableView.reloadData()
+        }
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("movie cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let movie = movies![indexPath.row]
+        
+        cell.textLabel?.text = movie["title"] as? String
+        return cell
+    }
+    
+}
